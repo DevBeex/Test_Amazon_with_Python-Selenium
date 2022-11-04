221c7dda1f3a4d4e174f2e971043f8ccee4a1308
Benjamin Rodriguez <benjamin.rodriguez3@utp.ac.pa>
2022-11-04 00:13:18 -0500

    Arreglo del ultimo paso para acceder al carrito de compra

--- prueba.py
+++ prueba.py
@@ -4,9 +4,8 @@ from selenium.webdriver.common.by import By
 import time
 
 driver = webdriver.Chrome('C://selenium-java-4.5.3/chromedriver.exe')
-#Hacemos la pestaña del navegador mas grande y eliminamos cookies
+#Hacemos la pestaña del navegador mas grande
 driver.maximize_window()
-driver.delete_all_cookies()
 
 #Comienza la prueba e identificamos los lugares del HTML de interés por ID y clase 
 driver.get("https://www.amazon.com/")
@@ -16,7 +15,7 @@ driver.find_element(By.CLASS_NAME, "a-price-whole").click()
 driver.find_element(By.CLASS_NAME, "a-button-inner").click()
 driver.find_element(By.ID, "quantity_1").click()
 driver.find_element(By.ID, "add-to-cart-button").click()
-driver.find_element(By.CLASS_NAME, "a-button-inner").click()
+driver.find_element(By.ID, "sw-gtc").click()
 #Termina la prueba
 
 time.sleep(100)
